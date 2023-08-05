308468a6aa90299cda591df133b6a7a2c9f424c3
MaybeCanadian <1coffeye@gmail.com>
2023-08-04 20:00:32 -0400

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    #region Event Dispatchers
+    public delegate void UpdateEvent(float delta);
+
+    public static UpdateEvent UpdateTick;
+    public static UpdateEvent FixedUpdateTick;
+    public static UpdateEvent LateUpdateTick;
+    #endregion
+
+    #region Update Functions
+    private void Update()
+    {
+        UpdateTick?.Invoke(Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        FixedUpdateTick?.Invoke(Time.fixedDeltaTime);
+    }
+    private void LateUpdate()
+    {
+        LateUpdateTick?.Invoke(Time.deltaTime);
+    }
+    #endregion
+
+}
